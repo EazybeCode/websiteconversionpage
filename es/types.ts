195e0f133a0c90f89022a7819d99926be307c060
tech933 <tech@eazybe.com>
2026-01-20 15:47:44 +0530

    Delete es directory

--- es/types.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Feature {
-  id: string;
-  tag: string;
-  title: string;
-  description: string;
-  points: string[];
-  visualType: 'timeline' | 'inbox' | 'workflow' | 'properties';
-}
